7e34f92e7fcca21b499caa9771b74e96a8eae5c6
Tanuja <tanujapatel29@gmail.com>
2025-07-07 17:18:36 +1000

    new build

--- src/components/project/Home.jsx
+++ src/components/project/Home.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 export default function Home() {
@@ -14,12 +15,7 @@ export default function Home() {
         <div className="relative flex flex-col lg:flex-row justify-left items-center lg:space-x-8">
           {/* Desktop Screen on Left */}
           <div className="w-full lg:w-1/2 relative">
-            <img
-<<<<<<< HEAD
-              src="./img/desktop.png"  // Replace with your desktop mockup image
-=======
-              src="/img/desktop.png"  // Replace with your desktop mockup image
->>>>>>> 2793386f5cf75d181e7d87553baa98e1722905ca
+            <img src="/img/desktop.png"  // Replace with your desktop mockup image
               alt="Desktop Mockup"
               className="w-full shadow-2xl rounded-lg"
             />
@@ -27,19 +23,11 @@ export default function Home() {
 
           {/* Text Section on Right */}
           <div className="w-full lg:w-1/2 text-left mt-8 lg:mt-0">
-<<<<<<< HEAD
-            <h3 className="text-2xl font-bold text-white">Responsive Design</h3>
-            <p className="text-white mt-4">
-              This portfolio is designed to be fully responsive, adapting to different screen sizes such as desktop, tablet, and mobile devices. Check out the interactive projects displayed on the screen.</p>
-            <button className="mt-6 px-6 py-3 bg-white text-purple-500 rounded-lg shadow-lg hover:bg-gray-200">
-=======
             <h3 className="text-2xl font-bold text-white">Hi, I am Tanuja Patel — Front-End Developer.</h3>
             <p className="text-white mt-4">
             I design and build responsive websites using React, Tailwind, and modern web technologies.
-I’m passionate about creating clean UIs and learning something new with every project.
-
- Open to junior dev roles and collaborations <button className="mt-6 px-6 py-3 bg-white text-purple-500 rounded-lg shadow-lg hover:bg-gray-200">
->>>>>>> 2793386f5cf75d181e7d87553baa98e1722905ca
+I’m passionate about creating clean UIs and learning something new with every project. Open to junior dev roles and collaborations 
+</p><button className="mt-6 px-6 py-3 bg-white text-purple-500 rounded-lg shadow-lg hover:bg-gray-200">
               View Projects
             </button>
           </div>
